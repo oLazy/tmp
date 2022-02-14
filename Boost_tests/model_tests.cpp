08047ac9a13ef5fe4f904075a8242c6b003eb45d
Eric Mandolesi <mandolesi.geo@gmail.com>
2022-02-14 08:05:27 +0000

    working on the cov test

--- Boost_tests/model_tests.cpp
+++ Boost_tests/model_tests.cpp
@@ -6,10 +6,25 @@
 BOOST_AUTO_TEST_SUITE(test_mt_model)
 BOOST_AUTO_TEST_CASE( cov0_vs_cov1 )
 {
-    BOOST_CHECK_EQUAL(1,1);
-}
-    BOOST_AUTO_TEST_CASE( cov1_vs_cov1 )
-    {
-        BOOST_CHECK_EQUAL(1,1);
+    //Generate a model
+    mtobj::model m;
+    m.nodes.push_back({0,-1});
+    m.nodes.push_back({2000,-3,-2,45.});
+    m.nodes.push_back({6000,-1,-3,-15.});
+    m.calc_params();
+    //Generate Dataset
+    mtobj::Dataset dataset;
+    for (auto i=0; i<31; i++){
+        double increment = 5./30.;
+        double per = pow(10,-2+static_cast<double>(i)*increment);
+        dataset[per] = m(per);
     }
+    //Generate two equivalent covariances
+    mtobj::Cov0 cov0;
+    mtobj::Cov1 cov1;
+    for (auto it : dataset){
+        auto T = it.first;
+        
+    }
+}
 BOOST_AUTO_TEST_SUITE_END()
